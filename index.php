54494bb337093ede3ef1efb87b348f1395be0c3a
johmark-123 <johnmarkgr174@gmail.com>
2025-07-25 11:47:50 +0800

    Add files via upload
    Simple Student Grading System

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Gitalan\Gs\Models\StudentModel;
+
+require 'vendor/autoload.php';
+
+$student = new StudentModel;
+
+$student->name="John doe";
+$student->course="BSIT";
+$student->year_level=4;
+$student-sectionn;
+
+$student = StudentModel->read();
+print_r($student);
